ac556713b4d4901caa494d37d7be2fa95a5a3566
Sehajpreet Singh <sehajmagan@gmail.com>
2024-11-11 20:14:39 -0500

    updated delete tasks

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 let tasks = [];
 
 app.get('/', (req, res) => {
-    res.render('tasks', { posts });
+    res.render('tasks', { tasks });
 })
 
 app.get('/new-task', (req, res) => {
@@ -19,7 +19,33 @@ app.get('/new-task', (req, res) => {
 
 
 app.post('/add-task', (req, res) => {
-    const { tasktitle } = req.body;
-    tasks.push(tasktitle);
+    const { description } = req.body;
+
+    let id = 0;
+    if (tasks.length) {
+        id = (parseInt(tasks[tasks.length - 1].id) + 1).toString();
+    } else {
+        id = (tasks.length + 1).toString();
+    }
+
+    const newTask = {
+        id,
+        description,
+    }
+    tasks.push(newTask);
     res.redirect('/');
 })
+
+app.post('/delete-task/:id', (req, res) => {
+    const taskId = req.params.id;
+    const task = tasks.find(p => p.id === taskId);
+    const index = tasks.indexOf(task);
+    if (index > -1) { // only splice array when item is found
+        tasks.splice(index, 1); // 2nd parameter means remove one item only
+    }
+    res.redirect('/');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
